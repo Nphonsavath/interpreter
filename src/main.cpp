9830c7a48edac522c2552aeb699f658c407aaa18
root <nphonsavath@gmail.com>
2025-01-19 15:50:37 -0600

    Having issues processing operator after number literal. -NP

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <cmath>
 #include <string>
 
 std::string read_file_contents(const std::string& filename);
+int isDigit(int character);
 
 int main(int argc, char *argv[]) {
     // Disable output buffering
@@ -26,105 +28,129 @@ int main(int argc, char *argv[]) {
         std::string file_contents = read_file_contents(argv[2]);
 	
 	for (std::size_t i = 0; i < file_contents.size(); i++) {
-		switch (file_contents[i]) { 
-			case '(': 
-				std::cout << "LEFT_PAREN ( null" << std::endl; break;
-			case ')': 
-				std::cout << "RIGHT_PAREN ) null" << std::endl; break;
-			case '{':
-				std::cout << "LEFT_BRACE { null" << std::endl; break;
-			case '}':
-				std::cout << "RIGHT_BRACE } null" << std::endl; break;
-			case '*':
-				std::cout << "STAR * null" << std::endl; break;
-			case '.':
-				std::cout << "DOT . null" << std::endl; break;
-			case ',':
-				std::cout << "COMMA , null" << std::endl; break;
-			case '+':
-				std::cout << "PLUS + null" << std::endl; break;
-			case '-':
-				std::cout << "MINUS - null" << std::endl; break;
-			case ';':
-				std::cout << "SEMICOLON ; null" << std::endl; break;
-			case '=':
-				if (file_contents[i+1] == '=') {
-					std::cout << "EQUAL_EQUAL == null" << std::endl;
+		if (isDigit(file_contents[i])) {
+			std::string result = "";
+			while (isDigit(file_contents[i])) {
+				result += file_contents[i];
 					i += 1;
-				} else {
-					std::cout << "EQUAL = null" << std::endl;
-				}
-				break;
-			case '!':
-				if (file_contents[i+1] == '=') {
-					std::cout << "BANG_EQUAL != null" << std::endl;
-					i += 1;
-				} else {
-					std::cout << "BANG ! null" << std::endl;
-				}
-				break;
-			case '<':
-				if (file_contents[i+1] == '=') {
-					std::cout << "LESS_EQUAL <= null" << std::endl;
-					i += 1;
-				} else {
-					std::cout << "LESS < null" << std::endl;
-				}
-				break;
-			case '>':
-				if (file_contents[i+1] == '=') {
-					std::cout << "GREATER_EQUAL >= null" << std::endl;
-					i += 1;
-				} else {
-					std::cout << "GREATER > null" << std::endl;
-				}
-				break;
-			case '/':
-				if (file_contents[i+1] == '/') {
-					//i += 1;
-					while (i < file_contents.size() && file_contents[i+1] != '\n') {
-						i++;
-					}
-				} else {
-					std::cout << "SLASH / null" << std::endl;
-				}
-				break;
-			case ' ':
-			case '\r':
-			case '\t': 
-				break;
-			case '\n':
-				line++;
-				break;
-			case '\"':
-				{
-				std::string result = "";
-				while (i < file_contents.size() && file_contents[i+1] != '\"') {
-					result += file_contents[i+1];
-					i += 1;
-				}
-				if (i == file_contents.size()) { 
-					std::cerr << "[line " << line << "] Error: Unterminated string." << std::endl;
-					ret_val = 65;
-				} else {
-					i += 1;
-					std::cout << "STRING \"" << result << "\" " << result << std::endl;
-				}
-				//i += 1;
-				break;
-				}
-			default:
-				std::cerr << "[line " << line << "] Error: Unexpected character: " << file_contents[i] << std::endl;
-				ret_val = 65;
-				break;
 			}
-		}
+			std::string resultFloat = result + ".0";
+			if (file_contents[i] != '.') {
+				std::cout << "NUMBER " << result << " " << resultFloat << std::endl;
+			} else {
+				result += '.';
+				i += 1;
+				while (isDigit(file_contents[i])) {
+					result += file_contents[i];
+					i += 1;
+				}
+				double number = std::stod(result);
+				if (std::fmod(number, 1.0) == 0.0) {
+					std::cout << "NUMBER " << result << " " << resultFloat << std::endl;
+				} else {
+					std::cout << "NUMBER " << result << " " << result << std::endl;
+				}
+			}
+		} else { 
+			switch (file_contents[i]) { 
+				case '(': 
+					std::cout << "LEFT_PAREN ( null" << std::endl; break;
+				case ')': 
+					std::cout << "RIGHT_PAREN ) null" << std::endl; break;
+				case '{':
+					std::cout << "LEFT_BRACE { null" << std::endl; break;
+				case '}':
+					std::cout << "RIGHT_BRACE } null" << std::endl; break;
+				case '*':
+					std::cout << "STAR * null" << std::endl; break;
+				case '.':
+					std::cout << "DOT . null" << std::endl; break;
+				case ',':
+					std::cout << "COMMA , null" << std::endl; break;
+				case '+':
+					std::cout << "PLUS + null" << std::endl; break;
+				case '-':
+					std::cout << "MINUS - null" << std::endl; break;
+				case ';':
+					std::cout << "SEMICOLON ; null" << std::endl; break;
+				case '=':
+					if (file_contents[i+1] == '=') {
+						std::cout << "EQUAL_EQUAL == null" << std::endl;
+						i += 1;
+					} else {
+						std::cout << "EQUAL = null" << std::endl;
+					}
+					break;
+				case '!':
+					if (file_contents[i+1] == '=') {
+						std::cout << "BANG_EQUAL != null" << std::endl;
+						i += 1;
+					} else {
+						std::cout << "BANG ! null" << std::endl;
+					}
+					break;
+				case '<':
+					if (file_contents[i+1] == '=') {
+						std::cout << "LESS_EQUAL <= null" << std::endl;
+						i += 1;
+					} else {
+						std::cout << "LESS < null" << std::endl;
+					}
+					break;
+				case '>':
+					if (file_contents[i+1] == '=') {
+						std::cout << "GREATER_EQUAL >= null" << std::endl;
+						i += 1;
+					} else {
+						std::cout << "GREATER > null" << std::endl;
+					}
+					break;
+				case '/':
+					if (file_contents[i+1] == '/') {
+						//i += 1;
+						while (i < file_contents.size() && file_contents[i+1] != '\n') {
+							i++;
+						}
+					} else {
+						std::cout << "SLASH / null" << std::endl;
+					}
+					break;
+				case ' ':
+				case '\r':
+				case '\t': 
+					break;
+				case '\n':
+					line++;
+					break;
+				case '\"':
+					{
+					std::string result = "";
+					while (i < file_contents.size() && file_contents[i+1] != '\"') {
+						result += file_contents[i+1];
+						i += 1;
+					}
+					if (i == file_contents.size()) { 
+						std::cerr << "[line " << line << "] Error: Unterminated string." << std::endl;
+						ret_val = 65;
+					} else {
+						i += 1;
+						std::cout << "STRING \"" << result << "\" " << result << std::endl;
+					}
+					break;
+					}
+				default:
+					std::cerr << "[line " << line << "] Error: Unexpected character: " << file_contents[i] << std::endl;
+					ret_val = 65;
+					break;
+				}
+			}
+		} 
 		std::cout << "EOF  null" << std::endl;
 		return ret_val;
-	    } else {
-		std::cerr << "Unknown command: " << command << std::endl;
-		return 1;
-	    }
+    } else {
+	std::cerr << "Unknown command: " << command << std::endl;
+	return 1;
+	}
 }
 
 std::string read_file_contents(const std::string& filename) {
@@ -139,4 +165,8 @@ std::string read_file_contents(const std::string& filename) {
     file.close();
 
     return buffer.str();
+}
+
+int isDigit(int character) {
+	return character >= '0' && character <= '9';
 }
